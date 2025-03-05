e68e4b28ebce3fd8f57f512372207e2a43774b86
Azweelias <eliasdawarpana@gmail.com>
2025-03-04 19:00:48 -0800

    Initial Commit

---
+++ FitFriend/FitFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFriendApp.swift
+//  FitFriend
+//
+//  Created by admin on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FitFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
